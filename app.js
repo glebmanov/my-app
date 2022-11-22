c8ad011f950a8c2fabc57d08038e5460d75f6ce1
Gleb Manov <glebmanov@gmail.com>
2022-11-22 19:16:35 +0300

    init

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const http = require('http')
+
+const app = express()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+  app.get('*', (_, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
+
+const start = async () => {
+  try {
+    http.createServer(app).listen(80, () => console.log(`App has been started on port 80...`))
+  } catch (e) {
+    console.log('Server Error', e.message)
+    process.exit(1)
+  }
+}
+
+start()
