e99934f00e0bdd1ea25f25fca88151fec1bd29e1
Gleb Manov <glebmanov@gmail.com>
2022-12-09 12:43:59 +0300

    add react hook form

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,16 +1,74 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const Auth = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm({
+    mode: 'onBlur',
+  })
+
+  const onSubmit = data => {
+    alert(JSON.stringify(data))
+    reset()
+  }
+
   return (
     <div className='container-auth'>
       <div className='auth'>
-        <input type='email' placeholder='Email' />
-        <input type='text' placeholder='Nikname' />
-        <input type='password' placeholder='Password' />
-        <div className='auth-buttons'>
-          <button className='btn btn-cstm btn-sign-in'>Sign in</button>
-          <button className='btn-sign-up'>Sign up</button>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label>
+            Nikname
+            <input
+              type='text'
+              {...register('name', {
+                required: 'field is required',
+                minLength: {
+                  value: 6,
+                  message: 'minimum length 6 characters',
+                },
+              })}
+            />
+          </label>
+          <div className='error'>{errors?.name && <p>{errors?.name?.message || 'Error'}</p>}</div>
+
+          <label>
+            Email
+            <input
+              type='email'
+              {...register('email', {
+                required: 'field is required',
+                pattern: /.+@.+..+/i,
+              })}
+            />
+          </label>
+          <div className='error'>{errors?.email && <p>{errors?.email?.message || 'Error'}</p>}</div>
+
+          <label>
+            Password
+            <input
+              type='password'
+              {...register('password', {
+                required: 'field is required',
+                minLength: {
+                  value: 6,
+                  message: 'minimum length 6 characters',
+                },
+              })}
+            />
+          </label>
+          <div className='error'>{errors?.password && <p>{errors?.password?.message || 'Error'}</p>}</div>
+
+          <div className='auth-buttons'>
+            <button type='submit' className='btn btn-cstm btn-sign-in'>
+              Sign in
+            </button>
+            <button className='btn-sign-up'>Sign up</button>
+          </div>
+        </form>
       </div>
     </div>
   )
