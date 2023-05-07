b1b6fc030a2c7fe54ba521d4ed0b1dfcaaf0c9be
Gleb Manov <glebmanov@gmail.com>
2023-05-07 09:16:26 +0300

    type fix

--- client/src/modules/Climbspots/components/Button.tsx
+++ client/src/modules/Climbspots/components/Button.tsx
@@ -1,23 +1,21 @@
 import React from 'react'
-import { useAppDispatch } from 'hooks/index'
 import cn from 'classnames'
-
+import { useAppDispatch } from 'hooks/useRedux'
 interface ButtonProps {
-  className?: string
-  name: string
   handler: any
   value: string | number
+  text: string
   isActive: boolean
 }
 
-const Button: React.FC<ButtonProps> = ({ className, name, handler, value, isActive }) => {
+const Button: React.FC<ButtonProps> = ({ text, handler, value, isActive }) => {
   const dispatch = useAppDispatch()
 
-  const classNames = cn(`btn btn-primary text-nowrap ${className} btn-cstm`, { active: isActive })
+  const classNames = cn('btn-cstm', { active: isActive })
 
   return (
     <button className={classNames} type='button' onClick={() => dispatch(handler(value))}>
-      <span>{name}</span>
+      <span>{text}</span>
     </button>
   )
 }
